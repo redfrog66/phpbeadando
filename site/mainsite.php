734c776226db50611601c91b70d3f583c8c1451c
Gellén <gellenrebeka@gmail.com>
2023-11-21 13:47:38 +0100

    harcolok a php-val (nem tetszik)

--- site/mainsite.php
+++ site/mainsite.php
@@ -54,15 +54,15 @@ h1 {
 
 </body>
 </html>
-
+// Készíts egy esztétikus PHP oldalt, amely egy formban bekéri a felhasználói nevet (Username) és a
+// jelszót (echo nélkül). A bevitt információkat GET vagy POST tömb segítségével juttasd át a szerver
+// oldalra.
 <?php
 require 'passwords/passwords.php';
 
 $users = getUsers();
 
-// Készíts egy esztétikus PHP oldalt, amely egy formban bekéri a felhasználói nevet (Username) és a
-// jelszót (echo nélkül). A bevitt információkat GET vagy POST tömb segítségével juttasd át a szerver
-// oldalra.
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     $username = $_POST['uname'];
